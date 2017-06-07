1fc59a4ba2cd4683b516cf6903a8ff9987d9e36e
andre <a.marcello92@gmail.com>
2017-06-07 03:14:54 -0300

    Ajustando estetica

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = '/media'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/andre/yanocuzao/ENV/mysite/polls/static/polls/media/'
+#MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
