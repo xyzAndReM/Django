6b828f194307357ac973aae7b701169f311b9c3a
andre <a.marcello92@gmail.com>
2017-06-07 07:51:03 -0300

    versao final

--- mysite/polls/static/polls/js/quizz.js
+++ mysite/polls/static/polls/js/quizz.js
@@ -8,10 +8,14 @@ var answer = $("input#answer").val()
 if(answer === str){
 $("#respond").removeClass("btn btn-danger");
 $("#respond").addClass("btn btn-success");
+$("#respond").text("Correto!");
+var audio = new Audio('/static/polls/media/sounds/correct.mp3');
+audio.play();
 }
 else{
 $("#respond").removeClass("btn btn-success");	
 $("#respond").addClass("btn btn-danger");
+$("#respond").text("Incorreto :(");
 }
 
 
